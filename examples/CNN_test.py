328feefda4524d808658cc6c879cd3d34b446633
Raj <rajassaikeerthi@gmail.com>
2025-01-30 17:11:00 -0500

    New Conv NN class testing file now showcases the classes' capabilities better

--- examples/CNN_test.py
+++ examples/CNN_test.py
@@ -1,28 +1,28 @@
 import pycustomnets as pc
-import numpy as np
-import PIL.Image
-import matplotlib.pyplot as pyp
-
 from keras._tf_keras.keras.datasets import mnist
 
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
-image_tensor = np.asarray(PIL.Image.open("PATH TO IMAGE HERE"))
-
 def main():
-    CNN = pc.ConvModel(1, 2)
-    CNN.setImage(image_tensor)
-    CNN.addLayerC(2, 1)
+    CNN = pc.ConvModel(100, 10)
+    CNN.setImage(x_train[0])
+    CNN.addLayerC(2, 1, 1)
+    CNN.addLayer(128, "lrelu")
     CNN.addLayer(10, "softmax")
-    CNN.setError("b_cross_entropy")
+    CNN.setError("bce")
     CNN.initializeC()
-    print(CNN.error([0]))
-    print(CNN.kernels)
-    for i in range(10):
-        CNN.setImage(image_tensor)
-        CNN.optimizeC([0], "adam")
-    print(CNN.error([0]))
-    print(CNN.kernels)
+    CNN.a_adam = 0.001
+    avg_error = 0
+    for i in range(1000):
+        avg_error += CNN.singlePredict(x_train[i], [y_train[i]], True)
+    avg_error_before = avg_error / 1000
+    CNN.train(x_train[0:1000], y_train[0:1000], "adam", "none")
+    avg_error = 0
+    for i in range(1000):
+        avg_error += CNN.singlePredict(x_train[i], [y_train[i]], True)
+    print(avg_error_before)
+    print(avg_error/1000)
+
 
 
 if __name__ == "__main__":
