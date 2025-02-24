57f2873c48ee3f2832a27ac0fe82f8baee6b551f
Raj <rajassaikeerthi@gmail.com>
2025-02-24 16:14:27 -0500

    Modified CNN example file to show proof of function in a more reasonable time frame

--- examples/CNN_test.py
+++ examples/CNN_test.py
@@ -4,30 +4,23 @@ from keras._tf_keras.keras.datasets import mnist
 (x_train, y_train), (x_test, y_test) = mnist.load_data()
 
 def main():
-    CNN = pc.ConvModel(100, 10)
+    CNN = pc.ConvModel(2, 4, True, 0.3)
     CNN.setImage(x_train[0])
-    CNN.addLayerC(2, 1, 1)
+    CNN.addLayerC(3, 1, 64)
+    #CNN.addLayerC(2, 1, 32)
     CNN.addLayer(128, "lrelu")
     CNN.addLayer(10, "softmax")
     CNN.setError("bce")
     CNN.initializeC()
     CNN.a_adam = 0.001
-    avg_error = 0
-    for i in range(1000):
-        avg_error += CNN.singlePredict(x_train[i], [y_train[i]], True)
-    avg_error_before = avg_error / 1000
-    CNN.train(x_train[0:1000], y_train[0:1000], "adam", "none")
-    avg_error = 0
-    for i in range(1000):
-        avg_error += CNN.singlePredict(x_train[i], [y_train[i]], True)
-    print(avg_error_before)
-    print(avg_error/1000)
+
+    CNN.singlePredict(x_test[8], [y_test[8]])
+
+    CNN.train(x_train[0:10], y_train[0:10], "adam")
+
+    CNN.singlePredict(x_test[8], [y_test[8]])
 
 
 
 if __name__ == "__main__":
     main()
-
-
-
-
