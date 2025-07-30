1e3769fd182b9610c9d8b10e35a62ab66afaeeda
Raj <rajassaikeerthi@gmail.com>
2025-07-30 16:54:15 -0400

    Update linearfunction.py

--- examples/linearfunction.py
+++ examples/linearfunction.py
@@ -1,3 +1,5 @@
+import time
+
 from pycustomnets import *
 
 def training_func(x):
@@ -12,26 +14,22 @@ def main():
         training_data_in.append([i + 1])
         training_data_out.append([training_func(i + 1)])
 
-    #print(training_data_out)
-    #print(training_data_in)
-
     epoches = 500
-    NN = ModelStandard()
+    NN = ModelStandard(1, 1, False, False)
     NN.setInput(training_data_in[0])
+    NN.setError("mse")
     NN.addLayer(1, "lrelu")
     NN.initialize()
     for _ in range(epoches):
         for i in range(len(training_data_in)):
-            NN.setInput(training_data_in[i])
-            #print("Actual: " + str(training_data_out[i]))
-            #NN.out()
-            print("Error: " + str(NN.error(training_data_out[i])))
-            NN.optimize(training_data_out[i], "adam")
+            NN.update(training_data_in[i], training_data_out[i], "sgd")
 
-    print("Predicting 50: " + str(training_func(50)))
-    NN.setInput([50])
-    NN.out()
+    print("Predicting 50 -> " + str(training_func(50)))
+    NN.singlePredict([50], [training_func(50)])
 
 
 if __name__ == "__main__":
+    start = time.time()
     main()
+    end = time.time()
+    print(end - start)
